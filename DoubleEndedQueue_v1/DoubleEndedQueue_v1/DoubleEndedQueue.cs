2460a062a89804f74ed174d756c47e95dd56bc40
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-04 12:44:59 +0430

    Add files via upload

---
+++ DoubleEndedQueue_v1/DoubleEndedQueue_v1/DoubleEndedQueue.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleEndedQueue_v1
+{
+    public class DoubleEndedQueue
+    {
+        int[] array;
+        int front = -1;
+        int rear;
+
+        public DoubleEndedQueue(int length)
+        {
+            array = new int[length];
+            rear = length;
+        }
+
+
+        public bool IsEmpty(string arrayName)
+        {
+            bool result = false;
+            switch (arrayName)
+            {
+                case "rear":
+                    if (rear >= array.Length)
+                        result = true;
+                    break;
+                case "front":
+                    if (front == -1)
+                        result = true;
+                    break;
+            }
+            return result;
+        }
+
+        public bool IsFull()
+        {
+            if (front + 1 == rear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void EnqueueFront(int pushItem)
+        {
+            if (!IsFull())
+            {
+                front++;
+                array[front] = pushItem;
+            }
+            else
+            {
+                Console.WriteLine("arrya is full to push to the front");
+            }
+        }
+
+        public void EnqueueBack(int item)
+        {
+            if (!IsFull())
+            {
+                rear--;
+                array[rear] = item;
+            }
+            else
+            {
+                Console.WriteLine("array is full to push to the back");
+            }
+        }
+
+
+        public void DequeueFront()
+        {
+            if (!IsEmpty("front"))
+            {
+                int result = array[front];
+                Console.WriteLine(result);
+                front--;
+            }
+            else
+            {
+                Console.WriteLine("array is empty to pop from front");
+            }
+
+        }
+
+        public void DequeuBack()
+        {
+            if (!IsEmpty("rear"))
+            {
+                int result = array[rear];
+                Console.WriteLine(result);
+                rear++;
+            }
+            else
+            {
+                Console.WriteLine("array is emtpy to pop from back");
+            }
+        }
+
+        public void PrintMembers()
+        {
+            if (!(rear == front))
+            {
+                for (int i = 0; i <= front; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+
+                for (int i = rear; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("array is emtpy to print");
+            }
+        }
+    }
+}
---
+++ DoubleEndedQueue_v1/DoubleEndedQueue_v1/Program.cs
@@ -0,0 +1,44 @@
+﻿using DoubleEndedQueue_v1;
+
+DoubleEndedQueue x = new DoubleEndedQueue(10);
+
+x.EnqueueFront(20);
+x.EnqueueFront(30);
+
+x.EnqueueBack(400);
+x.EnqueueBack(45);
+x.EnqueueBack(450);
+
+Console.WriteLine("Items : ");
+x.PrintMembers();
+Console.WriteLine();
+x.DequeuBack();
+x.DequeuBack();
+x.DequeuBack();
+x.DequeuBack(); // error 
+
+x.EnqueueBack(500);
+
+x.DequeuBack();
+
+x.DequeuBack(); // error 
+x.DequeuBack(); // error 
+x.DequeuBack(); // error 
+x.DequeuBack(); // error 
+x.DequeuBack(); // error 
+
+x.DequeueFront();
+x.DequeueFront();
+x.DequeueFront(); // error 
+
+x.EnqueueFront(500);
+x.DequeueFront();
+
+x.DequeueFront(); // error 
+x.DequeueFront(); // error 
+
+
+x.EnqueueBack(800);
+x.EnqueueFront(5000);
+Console.WriteLine("items");
+x.PrintMembers();
